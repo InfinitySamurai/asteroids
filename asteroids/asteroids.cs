c5ce94059049c7a92933165325813901cbfd274f
Russell Long <russell._long@hotmail.com>
2017-09-10 17:31:55 +1000

    Changed name from game1 to asteroids

--- asteroids/asteroids.cs
+++ asteroids/asteroids.cs
@@ -7,12 +7,12 @@ namespace asteroids
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class asteroids : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public asteroids()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
